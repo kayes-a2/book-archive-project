ea90368ea86f896e92c98a0c9fa5d7077edd6047
Kayes-a2 <you@example.com>
2021-09-02 22:10:28 +0600

    done

--- app-js/app.js
+++ app-js/app.js
@@ -1,18 +1,48 @@
+// add event handeler by anonymous function
+const error = document.getElementById('error-message');
+const searchInput = document.getElementById('inputField');
+const container = document.getElementById('container')
+
+
+
 document.getElementById('button').addEventListener('click', function () {
-  const search = document.getElementById('inputField').value;
+  const search = searchInput.value;
+  // clear search inputfield
+  searchInput.value = '';
+
+  container.textContent = "";
+  if (search === '') {
+    error.classList.add("p-2")
+    error.innerText = 'Search field can not be empty';
+    return
+  }
 
 
-  const url = (`http://openlibrary.org/search.json?q=${search}`)
+  // api link (load data) of book archive  
+  const url = (`https://openlibrary.org/search.json?q=${search}`)
+
+
+  // get api data by fetch
   fetch(url)
-    // console.log(url)
     .then(res => res.json())
-    .then(data => showData(data.docs));
+    .then(data => showData(data.docs))
 
+  search.value = "";
 
 })
 
-const showData = (data) => {
-  const container = document.getElementById('container')
+//show api data with arrow function
+const showData = data => {
+
+  // clear previous search value
+  container.textContent = "";
+  error.innerText = "";
+
+  if (data.length === 0) {
+    error.innerText = 'Book not found';
+    return;
+  }
+
   data.forEach(item => {
     const div = document.createElement('div');
     div.classList.add('col');
@@ -22,7 +52,7 @@ const showData = (data) => {
               <img  src=https://covers.openlibrary.org/b/id/${item.cover_i}-M.jpg class="card-img-top" alt="...">
             </div>
                   <div class="card-body">
-                      <h5 class="card-title">Book Name: ${item.title}</h5>
+                      <h5 class="card-title">Book Name:<span class='danger'>  ${item.title}</span></h5>
                       <h5 class="card-title">Author Name: ${item.author_name}</h5>
                       <h5 class="card-title">Publisher: ${item.publisher}</h5>
                       <h5 class="card-title">First Published Year: ${item.first_publish_year}</h5>
@@ -33,5 +63,4 @@ const showData = (data) => {
 
   })
 }
-const container = document.getElementById('div');
 
